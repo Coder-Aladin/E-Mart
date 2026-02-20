673fdc84814604717b4785004de7a5143f1abf71
Abdul Kadir <abdulkadir157350@gmail.com>
2026-02-20 23:03:12 +0530

    header compelete

---
+++ js/header.js
@@ -0,0 +1,19 @@
+const searchBtn = document.getElementById('search-btn');
+const searchBox = document.querySelector('.search-box');
+const menuOpen = document.getElementById('menu-btn');
+const menuClose = document.getElementById('close-panel');
+const menuBox = document.querySelector('.panel-2');
+
+searchBtn.addEventListener('click', () => {
+    searchBox.classList.toggle('active');
+    menuBox.classList.remove('active');
+});
+
+menuOpen.addEventListener('click', () => {
+    menuBox.classList.toggle('active');
+    searchBox.classList.remove('active');
+});
+
+menuClose.addEventListener('click', () => {
+    menuBox.classList.toggle('active');
+});
