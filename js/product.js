465f29590364d781b743724baaaf18626a95cfa0
Abdul Kadir <abdulkadir157350@gmail.com>
2026-03-09 16:52:08 +0530

    product fetching compelete

--- js/product.js
+++ js/product.js
@@ -1,51 +1,66 @@
-const cardContainer = document.querySelectorAll('.product-wrapper');
+const cardWrappers = document.querySelectorAll('.product-wrapper');
 
 window.onload = function () {
     fetchProducts();
 };
 
-const fetchProducts = async () => {
+async function fetchProducts() {
+    
+    for (const wrapper of cardWrappers) {
 
-    cardContainer.forEach(async (box) => {
-        
-        let url = `https://dummyjson.com/products/category/${box.getAttribute('id')}`
+        const category = wrapper.getAttribute('id');
 
-        const response = await fetch(url);
-        const data = await response.json();
-        
-        const info = data.products
+        const response = await fetch(`https://dummyjson.com/products/category/${category}`);
+        const productInfo = await response.json();
 
-        console.log(info);
-        
-        showingDetail(info)
-    });
+        const cards = productInfo.products
 
-}
+        cards.forEach(card => {
 
-const showingDetail = (info) => {
+            let productCard = `
+                <div class="product-card swiper-slide">
+                    <div class="saveBtn"><i class="material-symbols-outlined" id="save-icon">bookmark</i></div>
+                    <img src="${card.thumbnail}" alt="">
+                    <h3>${card.title}</h3>
+                    <p>product description</p>
+                    <p><i class="fa-solid fa-star"></i> 4.5</p>
+                    <p>discounts</p>
+                    <p>price</p>
+                    <div class="btn-box">
+                        <a href="" class="btn">shop now</a>
+                        <a href="" class="btn">add to cart</a>
+                    </div>
+                </div>
+            `
 
-    info.forEach(data => {
-        let productCard = `
-        <div class="product-card">
-            <div class="saveBtn"><i class="material-symbols-outlined" id="save-icon">bookmark</i></div>
-            <img src="${data.thumbnail}" alt="">
-            <h3>${data.title}</h3>
-            <p>product description</p>
-            <p><i class="fa-solid fa-star"></i> 4.5</p>
-            <p>discounts</p>
-            <p>price</p>
-            <div class="btn-box">
-                <a href="" class="btn">shop now</a>
-                <a href="" class="btn">add to cart</a>
-            </div>
-        </div>
-    `
-
-        cardContainer.forEach(cards => {
-            
-            cards.innerHTML += productCard
+            wrapper.innerHTML += productCard;
 
         });
-    });
+
+    }
 
 }
+
+
+var swiper = new Swiper(".sec-product .box-container", {
+  loop: true,
+  spaceBetween: 20,
+  centeredSlides: true,
+
+  autoplay: {
+    delay: 7500,
+    disableOnInteraction: false,
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1020: {
+      slidesPerView: 3,
+    },
+  },
+});
